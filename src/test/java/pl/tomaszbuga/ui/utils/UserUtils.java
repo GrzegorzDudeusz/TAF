fe1f84a8455ef57ec993b175c3696db706dc95ce
Grzegorz Dudek <grzegorzdudidudek@gmail.com>
2023-03-01 23:00:40 +0100

    Deleted unused comments Change getProperty for getenv in UserUtils.java class

--- src/test/java/pl/tomaszbuga/ui/utils/UserUtils.java
+++ src/test/java/pl/tomaszbuga/ui/utils/UserUtils.java
@@ -1,6 +1,9 @@
 package pl.tomaszbuga.ui.utils;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class UserUtils {
-    public static String username = System.getProperty("email");
-    public static String pass = System.getProperty("password");
+    public static String username = System.getenv("email");
+    public static String pass = System.getenv("password");
 }
